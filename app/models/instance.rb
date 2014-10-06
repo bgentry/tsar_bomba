56b89804ad205f163850c703758aeb561e2169c0
Blake Gentry <blakesgentry@gmail.com>
2014-10-05 23:59:23 -0700

    instance workflow

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,3 +1,19 @@
 class Instance < ActiveRecord::Base
+  include Workflow
+
   belongs_to :fleet
+
+  workflow_column :state
+  workflow do
+    state :awaiting_launch do
+      event :launch, :transitions_to => :launched
+    end
+    state :launched do
+      event :running, :transitions_to => :running
+    end
+    state :running do
+      event :bootstrap, :transitions_to => :ready
+    end
+    state :ready
+  end
 end
--- spec/models/instance_spec.rb
+++ spec/models/instance_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Instance, :type => :model do
+  let(:fleet)    { create(:fleet) }
+  let(:instance) { create(:instance, fleet: fleet) }
+
   it { should belong_to(:fleet) }
+
+  context 'workflow' do
+    it { should respond_to(:awaiting_launch?) }
+    it { should respond_to(:launch!) }
+
+    it { should respond_to(:launched?) }
+    it { should respond_to(:running!) }
+
+    it { should respond_to(:running?) }
+    it { should respond_to(:bootstrap!) }
+
+    it { should respond_to(:ready?) }
+
+    it "should begin with state awaiting_launch" do
+      expect(build_stubbed(:instance).awaiting_launch?).to eq(true)
+    end
+
+    it "should transition from awaiting_launch to launched" do
+      instance.launch!
+      expect(instance.launched?).to eq(true)
+    end
+
+    it "should transition from launched to running" do
+      instance.update_attribute(:state, "launched")
+      instance.running!
+      expect(instance.running?).to eq(true)
+    end
+
+    it "should transition from running to ready" do
+      instance.update_attribute(:state, "running")
+      instance.bootstrap!
+      expect(instance.ready?).to eq(true)
+    end
+  end
 end
