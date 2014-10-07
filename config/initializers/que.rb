d57b1e20390ac3b4ae524f00196b51697a518ead
Blake Gentry <blakesgentry@gmail.com>
2014-10-07 00:14:39 -0700

    no need to explicitly call for async mode, causes que worker to run even in rake tasks

--- config/initializers/que.rb
+++ config/initializers/que.rb
@@ -2,10 +2,5 @@
 # https://github.com/rails/rails/issues/17195
 ENV['QUE_QUEUE'] ||= 'default'
 
-# use async mode so we don't need to run separate worker processes
-unless Rails.env.test?
-  Que.mode = :async
-end
-
 # only sleep workers for 2 seconds instead of default 5
 Que.wake_interval = 2
