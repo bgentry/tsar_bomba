d81ffe3d8eafe578817c9cec08a3e5417d0e2039
Blake Gentry <blakesgentry@gmail.com>
2014-10-15 15:58:07 -0700

    add dns_name column to Instance model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015205638) do
+ActiveRecord::Schema.define(version: 20141015224001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141015205638) do
     t.string   "state"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "dns_name"
   end
 
   create_table "que_jobs", primary_key: "queue", force: true do |t|
